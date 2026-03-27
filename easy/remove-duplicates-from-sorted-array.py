0a7fcf2cfac857cd7173d491d36f56d0974b888f
Nikita200 <Ponomariovnikita02@gmail.com>
2026-03-27 22:22:46 +0100

    new solving

--- easy/remove-duplicates-from-sorted-array.py
+++ easy/remove-duplicates-from-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
         k = 1
 
         for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
+            if nums[i] != nums[i - 1]: 
                 nums[k] = nums[i]
                 k += 1
 
